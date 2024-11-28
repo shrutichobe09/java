2cae194a0484b4810a785fc27c748bccc50d42ff
shrutichobe09 <shruti.chobe@atyeti.com>
2024-11-28 16:01:06 +0530

    java program

--- hello_world.java
+++ hello_world.java
@@ -1,5 +1,7 @@
+package com.example;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World  java!");
+        System.out.println("Hello, World!");
     }
 }
